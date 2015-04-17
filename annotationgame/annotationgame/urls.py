8c7e87517828793e2f5275b9ae6bcb4a3e8018ef
Wessel Stoop <woseseltops@hotmail.com>
2015-04-17 15:19:09 +0200

    Show the stimulusview at root

--- annotationgame/annotationgame/urls.py
+++ annotationgame/annotationgame/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'annotationgame.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+     url(r'^$', 'main.views.stimulus'),
     url(r'^admin/', include(admin.site.urls)),
 ]
