b75c5772d80a141a555bc5e1e00bc9c1c8e61a5a
Wessel Stoop <woseseltops@hotmail.com>
2015-04-17 15:18:43 +0200

    Created a stimulus view

--- annotationgame/main/views.py
+++ annotationgame/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Answer
 
 # Create your views here.
+def stimulus(request):
+
+    return render(request,'stimulus.html',{'answers':[answer.text for answer in Answer.objects.all()]})
