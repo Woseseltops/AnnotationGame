cc86830a68170c2ca9b38888043a4103d0b8ed04
Wessel Stoop <woseseltops@hotmail.com>
2015-08-07 17:00:50 +0200

    Added functionality to calculate the highscore

--- annotationgame/main/models.py
+++ annotationgame/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from annotationgame.settings import HIGHSCORE_SIZE, MINIMUM_STREAK_FOR_HIGHSCORE
 
 # Create your models here.
 class Answer(models.Model):
@@ -20,3 +21,22 @@ class Streak(models.Model):
 
     def __str__(self):
         return self.playername+': '+str(self.length);
+
+def get_highscore():
+
+    streaks = Streak.objects.all()
+    return sorted(streaks,key=lambda x: x.length,reverse=True)[:HIGHSCORE_SIZE]
+
+def calculate_minimum_streak_to_enter_highscore():
+
+    highscore = get_highscore()
+
+    if len(highscore) < HIGHSCORE_SIZE:
+
+        #Equal to the score of the last one
+        return MINIMUM_STREAK_FOR_HIGHSCORE
+
+    else:
+
+        #Higher than the last one
+        return highscore[-1].length + 1
