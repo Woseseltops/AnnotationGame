4a35b808d03991f3119015822bc0e39bd4b39652
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-10-23 14:42:54 +0200

    Housekeeping

--- annotationgame/annotationgame/settings.py
+++ annotationgame/annotationgame/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/annotationgame/static/'
 STIMULI_FILE_LOCATION = BASE_DIR + '/stimuli.txt'
 
 HIGHSCORE_SIZE = 10
